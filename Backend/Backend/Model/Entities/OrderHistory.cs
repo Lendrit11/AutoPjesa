6b573b64a541d47944e33174896742bd58e7a0cc
Lendrit11 <lendritl122@gmail.com>
2025-07-27 20:42:05 +0200

    Kam krijuar modelin e OrderHistory

---
+++ Backend/Backend/Model/Entities/OrderHistory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class OrderHistory
+    {
+        [Key]
+        public int OrderHistoryId { get; set; }
+        public int orderId { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public DateOnly ChangedAt { get; set; }
+
+        public Order Order { get; set; } = null!; // Navigation property to Order entity
+    }
+}
