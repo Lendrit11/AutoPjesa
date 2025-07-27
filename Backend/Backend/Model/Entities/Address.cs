e98457132980a5edaee6c0ca704d3fed32cf5f3d
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-07-27 21:00:38 +0200

    kam krijuar entitetin e adreses

---
+++ Backend/Backend/Model/Entities/Address.cs
@@ -0,0 +1,21 @@
+﻿using AutoPjesa.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class Address
+    {
+        [Key]
+       public int AdressId { get; set; }
+        public int UserId { get; set; }
+        [Required]
+        public string AddressLine { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+          public AppUser User { get; set; } = null!;
+    }
+}
