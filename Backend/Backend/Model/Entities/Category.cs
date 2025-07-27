4c357bfb3abc600f6a0b3be4ed95ea757934bfde
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-07-27 21:04:32 +0200

    kam krijuar etitetin e kategoris se produketeve

---
+++ Backend/Backend/Model/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } 
+        public ICollection<Part> Parts { get; set; } = new List<Part>();
+    }
+}
