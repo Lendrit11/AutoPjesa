81c359d8eb40c40f9a17d3e10b8ee630b573fc0e
Lendrit11 <lendritl122@gmail.com>
2025-07-27 20:51:28 +0200

    Kam krijuar modelin e UserRole

---
+++ Backend/Backend/Model/Entities/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using AutoPjesa.Domain.Entities;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class UserRole
+    {
+        public int userId { get; set; }
+        public int roleId { get; set; }
+        public AppUser User { get; set; } = null!; // Navigation property to AppUser 
+        public Role Role { get; set; } = null!; // Navigation property to Role entity
+    }
+}
