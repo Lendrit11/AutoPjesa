ce88e11138dd946bd81e35873d2ee9ff5a031663
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-07-27 21:02:45 +0200

    kam krijaur entitetin e Cart

---
+++ Backend/Backend/Model/Entities/Cart.cs
@@ -0,0 +1,16 @@
+﻿using AutoPjesa.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Status { get; set; } // e.g., "Pending", "Completed", "Cancelled"
+        public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+        public AppUser User { get; set; } =null!; // Navigation property to AppUser 
+    }
+}
