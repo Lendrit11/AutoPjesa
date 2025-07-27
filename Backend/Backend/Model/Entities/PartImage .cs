067fbe27bb7f4fa239612e5284263fb0ab5320a6
Lendrit11 <lendritl122@gmail.com>
2025-07-27 20:47:24 +0200

    Kam krijuar modelin e PartImage

---
+++ Backend/Backend/Model/Entities/PartImage .cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class PartImage
+    {
+        [Key]
+        public int ImgId { get; set; }
+        [Required]
+        public int PartId { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string ImgUrl { get; set; } 
+        public bool IsPrimary { get; set; } = false;
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+        public Part Part { get; set; } = null!; // Navigation property to Part
+    }
+}
