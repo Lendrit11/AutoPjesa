023b11396eccd2ece031bd3e72891fd8ff689713
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-07-27 21:06:03 +0200

    kam krijuar etitetin Order

---
+++ Backend/Backend/Model/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using AutoPjesa.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+      
+        public int UserId { get; set; }
+        [Required]
+        public DateOnly OrderDate { get; set; }
+        [Required]
+        public string OrderStatus { get; set; } // e.g., "Pending", "Shipped", "Delivered"
+        [Required]
+        public decimal TotalAmount { get; set; } // Total amount for the order
+        public AppUser User { get; set; } = null!; // Navigation property to AppUser 
+
+        public ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+        public ICollection<OrderHistory> OrderHistories { get; set; } = new List<OrderHistory>();
+    }
+}
