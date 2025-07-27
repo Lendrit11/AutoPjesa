c6877b619e4a3a9cd69a8b3b6795162e79c75ce9
Lendrit11 <lendritl122@gmail.com>
2025-07-27 20:50:00 +0200

    Kam krijuar modelin e Stock

---
+++ Backend/Backend/Model/Entities/Stock.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoPjesa.Domain.Entities
+{
+    public class Stock
+    {
+        public int StockId { get; set; }
+        public int PartId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }= 0;
+        public DateTime LastUpdated { get; set; }
+        // Navigation property to Part entity
+        public Part Part { get; set; } = null!;
+        // Constructor to initialize LastUpdated to current time
+     
+    }
+}
