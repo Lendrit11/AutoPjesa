694116c4785be068e996d9dec77f2c12658f5112
Lendrit11 <lendritl122@gmail.com>
2025-07-27 20:49:03 +0200

    Kam krijuar modelin e Role

---
+++ Backend/Backend/Model/Entities/Role.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+    }
+}
