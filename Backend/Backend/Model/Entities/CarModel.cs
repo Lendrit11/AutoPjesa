41dc7e32d17b57b049048870163ffc2859a8fc46
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-07-27 21:01:33 +0200

    kam krijuar entietin e CarModel

---
+++ Backend/Backend/Model/Entities/CarModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class CarModel
+    {
+        [Key]
+        public int CarModelId { get; set; }
+        [Required]
+        public int ManufacturerId { get; set; }
+        [Required]
+        public string modelName { get; set; } 
+       public DateOnly YearStart { get; set; }
+        public DateOnly? YearEnd { get; set; }
+        public ICollection<PartCarModel> PartCarModels { get; set; } = new List<PartCarModel>();
+        public Manufacturer Manufacturer { get; set; } = null!; // Navigation property to Manufacturer entity
+    }
+}
