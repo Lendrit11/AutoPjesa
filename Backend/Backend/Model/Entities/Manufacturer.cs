1bf23d322ccc5471a44c74660543139e0a2935e7
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-07-27 21:05:22 +0200

    kam krijuar etitetin Manufacturer

---
+++ Backend/Backend/Model/Entities/Manufacturer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class Manufacturer
+    {
+        [Key]
+        public int ManufacturerId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Country { get; set; }
+        public ICollection<CarModel> CarModels { get; set; } = new List<CarModel>();
+    }
+}
