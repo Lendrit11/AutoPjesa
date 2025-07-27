f2ede2c0596dd2d4221c78d6c3aa7875fa5d627e
Lendrit11 <lendritl122@gmail.com>
2025-07-27 20:45:29 +0200

    Kam krijuar modelin e PartCarModel

---
+++ Backend/Backend/Model/Entities/PartCarModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoPjesa.Domain.Entities
+{
+    public class PartCarModel
+    {
+        public int PartId { get; set; }
+        public int CarModelId { get; set; }
+
+        public Part Part { get; set; } = null!; // Navigation property to Part entity
+        public CarModel CarModel { get; set; } = null!; // Navigation property to CarModel entity
+    }
+}
