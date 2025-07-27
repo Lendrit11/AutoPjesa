4644336c4c9bc04144a767b95f40968e342213ff
Lendrit11 <lendritl122@gmail.com>
2025-07-27 20:39:38 +0200

    Kam krijuar modelin e orderdetial

---
+++ Backend/Backend/Model/Entities/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int PartId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public Order Order { get; set; } = null!; // Navigation property to Order entity
+
+        public Part Part { get; set; } = null!; // Navigation property to Part entity
+    }
+}
