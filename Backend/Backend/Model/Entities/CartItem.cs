34b779a292fa59ad77ea1cb4685ed994f593cca3
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-07-27 21:03:31 +0200

    kam krijuar entitetin e CartItem

---
+++ Backend/Backend/Model/Entities/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesa.Domain.Entities
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+        public int CartId { get; set; }
+        public int PartId { get; set; }
+        public int Quantity { get; set; }
+
+        public Cart Cart { get; set; } = null!; // Navigation property to Cart
+        public Part Part { get; set; } = null!; // Navigation property to Part
+    }
+}
