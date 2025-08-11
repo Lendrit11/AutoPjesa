443ca9d65fb44b10346be01f1df4ce919b2a634f
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-08-11 19:01:32 +0200

    kemi krijuar edhe pjesen e blogeve ne databaze (per shtimin e lajmeve ne web-aplikacion)

---
+++ AutoPjesaa/AutoPjesaa/model/Entities/Blog.cs
@@ -0,0 +1,14 @@
+﻿using AutoPjesa.Domain.Entities;
+
+namespace AutoPjesaa.model.Entities
+{
+    public class Blog
+    {
+        public int blogId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public int UserId { get; set; }
+        public AppUser User { get; set; } = null!; // Navigation property to AppUser
+    }
+}
