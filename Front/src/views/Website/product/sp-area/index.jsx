e28e45b46038289124e31c930998630c49bd62a6
Jahirq1 <Jahirqoqaj8@gmail.com>
2025-08-14 22:16:42 +0200

    kam bere nderlidhjen ne mes te komponentit spa(single-product-area) me api endpoint

--- Front/src/views/Website/product/sp-area/index.jsx
+++ Front/src/views/Website/product/sp-area/index.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useRef } from 'react';  // Import React dhe hooks për efekt dhe referenca
-import $ from 'jquery';  // Import jQuery
-import 'slick-carousel/slick/slick.css';  // Stilet bazë të Slick Carousel
-import 'slick-carousel/slick/slick-theme.css';  // Tema për Slick Carousel
-import 'slick-carousel';  // Script i Slick Carousel
+import React, { useEffect, useRef } from 'react';
+import $ from 'jquery';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-// Import të ndryshme CSS vendor dhe plugins për stilizim
+// CSS
 import '../../../../assets/css/vendor/bootstrap.min.css';
 import '../../../../assets/css/vendor/font-awesome.css';
 import '../../../../assets/css/vendor/fontawesome-stars.css';
@@ -16,11 +19,32 @@ import '../../../../assets/css/plugins/lightgallery.min.css';
 import '../../../../assets/css/plugins/nice-select.css';
 import '../../../../assets/css/style.css';
 
-// Komponenti funksional për seksionin e produktit të vetëm
-const Single_Product_area = () => {
-  // Referenca për slider-in kryesor dhe slider-in e navigimit
+const Single_Product_area = ({ product }) => {
   const sliderMainRef = useRef(null);
   const sliderNavRef = useRef(null);
+
+  // 🧡 Shto në Favorites
+  const handleAddToFavorites = () => {
+    axios.post(
+      'http://localhost:5298/api/user/product/favorites/add',
+      { partId: product.id },
+      { withCredentials: true }
+    )
+      .then(() => {
+        toast.success("Produkti u shtua në favorite!");
+      })
+      .catch(err => {
+        if (err.response?.status === 401) {
+          toast.warn("Ju lutemi kyçuni për të shtuar në favorite.");
+        } else if (err.response?.status === 400) {
+          toast.info("Ky produkt është tashmë në favorite.");
+        } else {
+          toast.error("Ndodhi një gabim. Provoni përsëri.");
+        }
+      });
+  };
+
+  // ⚙️ Slick slider initialization
   useEffect(() => {
     if (sliderMainRef.current && sliderNavRef.current) {
       try {
@@ -34,6 +58,7 @@ const Single_Product_area = () => {
             asNavFor: sliderNavRef.current,
           });
         }
+
         if (!$(sliderNavRef.current).hasClass('slick-initialized')) {
           $(sliderNavRef.current).slick({
             slidesToShow: 3,
@@ -44,142 +69,146 @@ const Single_Product_area = () => {
             nextArrow: $('.custom-next'),
             responsive: [
               { breakpoint: 1501, settings: { slidesToShow: 3 } },
-              { breakpoint: 992, settings: { slidesToShow: 4 } },
-              { breakpoint: 768, settings: { slidesToShow: 3 } },
+              { breakpoint: 992, settings: { slidesToShow: 3 } },
+              { breakpoint: 768, settings: { slidesToShow: 2 } },
               { breakpoint: 575, settings: { slidesToShow: 2 } },
             ],
           });
         }
       } catch (err) {
-        console.error("Error initializing slick:", err);
+        console.error("Gabim gjatë slick init:", err);
       }
     }
+
     return () => {
-      if (sliderMainRef.current && $(sliderMainRef.current).hasClass('slick-initialized')) {
+      if ($(sliderMainRef.current).hasClass('slick-initialized')) {
         $(sliderMainRef.current).slick('unslick');
       }
-      if (sliderNavRef.current && $(sliderNavRef.current).hasClass('slick-initialized')) {
+      if ($(sliderNavRef.current).hasClass('slick-initialized')) {
         $(sliderNavRef.current).slick('unslick');
       }
     };
-  }, []);
-  
-    // Array bosh: hook ekzekutohet vetëm një herë kur komponenti mount-ohet
+  }, [product]);
+
+  if (!product) return null;
 
   return (
     <div className="sp-area">
       <div className="container-fluid">
         <div className="sp-nav">
           <div className="row">
-            {/* Kolona e majtë me slider-in e imazheve */}
+
+            {/* Imazhet */}
             <div className="col-lg-4">
               <div className="sp-img_area">
-                
                 <div className="sp-img_slider uren-slick-slider" ref={sliderMainRef}>
-                  {[1, 2, 3, 4, 5, 6].map((num, index) => (
-                    <div key={index} className="single-slide zoom">
-                      <img src={`assets/images/product/large-size/${num}.jpg`} alt="Uren's Product" />
+                  {[product.primaryImage, ...(product.otherImages || [])].map((img, idx) => (
+                    <div key={idx} className="single-slide zoom">
+                      <img src={img} alt={`Product ${idx}`} />
                     </div>
                   ))}
                 </div>
 
                 <div className="sp-img_slider-nav_wrapper">
-  {/* Custom Arrows */}
-  <button type="button" className="custom-prev">
-    <i className="fa fa-chevron-left"></i>
-  </button>
-  <button type="button" className="custom-next">
-    <i className="fa fa-chevron-right"></i>
-  </button>
+                  <button type="button" className="custom-prev">
+                    <i className="fa fa-chevron-left"></i>
+                  </button>
+                  <button type="button" className="custom-next">
+                    <i className="fa fa-chevron-right"></i>
+                  </button>
 
-  {/* Slider Nav */}
-  <div className="sp-img_slider-nav uren-slick-slider slider-navigation_style-3" ref={sliderNavRef}>
-    {[1, 2, 3, 4, 5, 6].map((num, index) => (
-      <div key={index} className="single-slide">
-        <img src={`assets/images/product/small-size/${num}.jpg`} alt="Uren's Thumbnail" />
-      </div>
-    ))}
-  </div>
-</div>
+                  <div className="sp-img_slider-nav uren-slick-slider slider-navigation_style-3" ref={sliderNavRef}>
+                    {[product.primaryImage, ...(product.otherImages || [])].map((img, idx) => (
+                      <div key={idx} className="single-slide">
+                        <img src={img} alt={`Thumb ${idx}`} />
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
 
-            {/* Kolona e djathtë me përmbajtje të produktit */}
+            {/* Detajet */}
             <div className="col-lg-8">
               <div className="sp-content">
                 <div className="sp-heading">
-                  <h5><a href="#">Dolorem odio provident ut nihil</a></h5>
+                  <h5>{product.name}</h5>
                 </div>
-                <span className="reference">Reference: demo_1</span>
+                <span className="reference">Product Code: {product.code || 'N/A'}</span>
+
                 <div className="rating-box">
                   <ul>
-                    {[...Array(3)].map((_, i) => (
-                      <li key={i}><i className="ion-android-star"></i></li>
-                    ))}
-                    {[...Array(2)].map((_, i) => (
-                      <li key={i} className="silver-color"><i className="ion-android-star"></i></li>
+                    {[...Array(5)].map((_, i) => (
+                      <li key={i}>
+                        <i className={`ion-android-star ${i < (product.rating || 0) ? '' : 'silver-color'}`}></i>
+                      </li>
                     ))}
                   </ul>
                 </div>
+
                 <div className="sp-essential_stuff">
                   <ul>
-                    <li>Brands <a href="#">Buxton</a></li>
-                    <li>Product Code: <a href="#">Product 16</a></li>
-                    <li>Reward Points: <a href="#">100</a></li>
-                    <li>Availability: <a href="#">In Stock</a></li>
-                    <li>EX Tax: <a href="#"><span>$453.35</span></a></li>
-                    <li>Price in reward points: <a href="#">400</a></li>
+                    <li>Availability: <a href="#">{product.available ? 'In Stock' : 'Out of Stock'}</a></li>
                   </ul>
                 </div>
-                <div className="product-size_box">
-                  <span>Size</span>
-                  {/* Dropdown për madhësitë */}
-                  <select className="myniceselect nice-select">
-                    <option value="1">S</option>
-                    <option value="2">M</option>
-                    <option value="3">L</option>
-                    <option value="4">XL</option>
-                  </select>
-                </div>
+
                 <div className="quantity">
                   <label>Quantity</label>
                   <div className="cart-plus-minus">
-                    {/* Input për sasinë */}
                     <input className="cart-plus-minus-box" defaultValue="1" type="text" />
                     <div className="dec qtybutton"><i className="fa fa-angle-down"></i></div>
                     <div className="inc qtybutton"><i className="fa fa-angle-up"></i></div>
                   </div>
                 </div>
+
                 <div className="qty-btn_area">
                   <ul>
-                    <li><a className="qty-cart_btn" href="cart.html">Add To Cart</a></li>
-                    <li><a className="qty-wishlist_btn" href="wishlist.html" title="Add To Wishlist"><i className="ion-android-favorite-outline"></i></a></li>
-                    <li><a className="qty-compare_btn" href="compare.html" title="Compare This Product"><i className="ion-ios-shuffle-strong"></i></a></li>
+                    <li><a className="qty-cart_btn" href="#">Add To Cart</a></li>
+                    <li>
+                      <button className="qty-wishlist_btn" title="Add To Wishlist" onClick={handleAddToFavorites}>
+                        <i className="ion-android-favorite-outline"></i> Add to Favorites
+                      </button>
+                    </li>
                   </ul>
+                </div>
+                <div className="sp-price mt-3">
+                  <h2 style={{ color: '#ff8800ff' }}>Price: €{product.price?.toFixed(2)}</h2>
+                  {product.oldPrice && (
+                    <h6 className="text-muted text-decoration-line-through">Old Price: €{product.oldPrice?.toFixed(2)}</h6>
+                  )}
                 </div>
                 <div className="uren-tag-line">
                   <h6>Tags:</h6>
-                  <a href="#">vehicle</a>, <a href="#">car</a>, <a href="#">bike</a>
+                  {product.tags?.length ? (
+                    product.tags.map((tag, idx) => (
+                      <React.Fragment key={idx}>
+                        <a href="#">{tag}</a>{idx < product.tags.length - 1 ? ', ' : ''}
+                      </React.Fragment>
+                    ))
+                  ) : (
+                    <span>No tags</span>
+                  )}
                 </div>
+
                 <div className="uren-social_link">
                   <ul>
-                    {/* Ikonat e rrjeteve sociale me link dhe target _blank */}
-                    <li className="facebook"><a href="https://www.facebook.com/" target="_blank" title="Facebook"><i className="fab fa-facebook"></i></a></li>
-                    <li className="twitter"><a href="https://twitter.com/" target="_blank" title="Twitter"><i className="fab fa-twitter-square"></i></a></li>
-                    <li className="youtube"><a href="https://www.youtube.com/" target="_blank" title="Youtube"><i className="fab fa-youtube"></i></a></li>
-                    <li className="google-plus"><a href="https://www.plus.google.com/discover" target="_blank" title="Google Plus"><i className="fab fa-google-plus"></i></a></li>
-                    <li className="instagram"><a href="https://rss.com/" target="_blank" title="Instagram"><i className="fab fa-instagram"></i></a></li>
+                    <li className="facebook"><a href="#"><i className="fab fa-facebook"></i></a></li>
+                    <li className="twitter"><a href="#"><i className="fab fa-twitter-square"></i></a></li>
+                    <li className="youtube"><a href="#"><i className="fab fa-youtube"></i></a></li>
+                    <li className="instagram"><a href="#"><i className="fab fa-instagram"></i></a></li>
                   </ul>
                 </div>
+
+
               </div>
             </div>
 
           </div>
         </div>
       </div>
+      <ToastContainer position="top-right" autoClose={3000} />
     </div>
   );
 };
 
-// Eksporton komponentin për t'u përdorur në pjesë të tjera të aplikacionit
 export default Single_Product_area;
