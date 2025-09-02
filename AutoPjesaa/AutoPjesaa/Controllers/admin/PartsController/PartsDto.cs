2f64f1f8da55c69c155e45704b0790d326ee5947
Lendrit11 <lendritl122@gmail.com>
2025-09-03 01:51:12 +0200

    kam shtuar nje api endpoint qe krijon pjes te reja me stok ,image dhe model /api/Parts supply(admin)

---
+++ AutoPjesaa/AutoPjesaa/Controllers/admin/PartsController/PartsDto.cs
@@ -0,0 +1,18 @@
+﻿namespace AutoPjesaa.model.DTO.Admin.PartsController;
+
+public class PartsDto
+{
+    public string Name { get; set; } = null!;
+    public string PartNumber { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Manufacturer { get; set; } = null!;
+    public int CategoryId { get; set; }
+    public int CompatibleFromYear { get; set; }
+    public int CompatibleToYear { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public int ReorderLevel { get; set; }
+    public decimal Discount { get; set; } = 0;
+    public string? ImageUrl { get; set; }
+    public List<int>? CompatibleModelIds { get; set; }
+}
