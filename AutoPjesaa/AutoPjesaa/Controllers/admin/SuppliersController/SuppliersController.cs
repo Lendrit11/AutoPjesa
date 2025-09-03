34dc5c99715f3390441f3af4bdc8fff2d2dd59a5
Lendrit11 <lendritl122@gmail.com>
2025-09-03 02:06:37 +0200

    Kam krijuar GET /api/Suppliers/{id}: per marrjen e nje furnizuesi edhe POST /api/Suppliers: per krijimin e nje furnzuesi tek supply(admin)

---
+++ AutoPjesaa/AutoPjesaa/Controllers/admin/SuppliersController/SuppliersController.cs
@@ -0,0 +1,39 @@
+﻿using AutoPjesa.Domain.Entities;
+using AutoPjesa.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoPjesaa.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly AutoPjesaDbContext _context;
+
+        public SuppliersController(AutoPjesaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            return await _context.Suppliers
+                .Include(s => s.SupplierManufacturers)
+                .ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            var supplier = await _context.Suppliers
+                .Include(s => s.SupplierManufacturers)
+                .FirstOrDefaultAsync(s => s.SupplierId == id);
+
+            if (supplier == null)
+                return NotFound();
+
+            return supplier;
+        }
+    }
+}
