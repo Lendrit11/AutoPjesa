1346d876544d96e94812554074c59bc0e693e983
Lendrit11 <lendritl122@gmail.com>
2025-09-03 02:30:26 +0200

    supplyAdmin(Backend): kam perfundur nje  CRUD me marrëdhëniet SupplierManufacturers per Prodhuesit
    - Implemento GET për të gjithë prodhuesit me marrëdhëniet e furnitorëve
    - Implemento GET për një prodhues specifik me ID
    - Shto POST endpoint për krijimin e prodhuesve të rinj
    - Shto PUT endpoint për përditësimin e prodhuesve ekzistues
    - Implemento DELETE endpoint me trajtimin e duhur të gabimeve
    - Përfshij validimin e ModelState dhe kontrollin e ekzistencës

--- AutoPjesaa/AutoPjesaa/Controllers/admin/ManufacturersController/ManufacturersController.cs
+++ AutoPjesaa/AutoPjesaa/Controllers/admin/ManufacturersController/ManufacturersController.cs
@@ -15,6 +15,8 @@ namespace AutoPjesaa.Controllers
         {
             _context = context;
         }
+
+        // GET: api/Manufacturers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Manufacturer>>> GetManufacturers()
         {
@@ -22,8 +24,10 @@ namespace AutoPjesaa.Controllers
                 .Include(m => m.SupplierManufacturers)
                 .ToListAsync();
         }
+
+        // GET: api/Manufacturers/5
         [HttpGet("{id}")]
- public async Task<ActionResult<Manufacturer>> GetManufacturer(int id)
+        public async Task<ActionResult<Manufacturer>> GetManufacturer(int id)
         {
             var manufacturer = await _context.Manufacturers
                 .Include(m => m.SupplierManufacturers)
@@ -35,6 +39,7 @@ namespace AutoPjesaa.Controllers
             return manufacturer;
         }
 
+        // POST: api/Manufacturers
         [HttpPost]
         public async Task<ActionResult<Manufacturer>> CreateManufacturer(Manufacturer manufacturer)
         {
@@ -46,6 +51,48 @@ namespace AutoPjesaa.Controllers
 
             return CreatedAtAction(nameof(GetManufacturer), new { id = manufacturer.ManufacturerId }, manufacturer);
         }
-    }
 
+        // PUT: api/Manufacturers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateManufacturer(int id, Manufacturer manufacturer)
+        {
+            if (id != manufacturer.ManufacturerId)
+                return BadRequest("ID mismatch");
+
+            _context.Entry(manufacturer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ManufacturerExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Manufacturers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteManufacturer(int id)
+        {
+            var manufacturer = await _context.Manufacturers.FindAsync(id);
+            if (manufacturer == null)
+                return NotFound();
+
+            _context.Manufacturers.Remove(manufacturer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ManufacturerExists(int id)
+        {
+            return _context.Manufacturers.Any(e => e.ManufacturerId == id);
+        }
+    }
 }
