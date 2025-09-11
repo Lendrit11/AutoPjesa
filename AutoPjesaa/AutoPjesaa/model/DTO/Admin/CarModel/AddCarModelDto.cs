6a51d97e5b7853bd6d1d593c1f59b4340d66158e
Lendrit11 <lendritl122@gmail.com>
2025-09-12 00:26:03 +0200

    Kam ber implementimin e Dto per CarModel

---
+++ AutoPjesaa/AutoPjesaa/model/DTO/Admin/CarModel/AddCarModelDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoPjesaa.model.DTO.Admin.CarModel
+{
+    public class AddCarModelDto
+    {
+        public string ModelName { get; set; } = null!;
+        public int ManufacturerId { get; set; }
+        public int? YearStart { get; set; }
+        public int? YearEnd { get; set; }
+    }
+}
