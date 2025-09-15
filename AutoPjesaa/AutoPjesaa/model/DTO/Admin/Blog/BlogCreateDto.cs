4da2b8247f098c1ce379674e967c8edd216c9d97
Lendrit11 <lendritl122@gmail.com>
2025-09-15 15:29:09 +0200

    Shtova DTO për krijimin dhe përditësimin e blogut Admin(Backend)

---
+++ AutoPjesaa/AutoPjesaa/model/DTO/Admin/Blog/BlogCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPjesaa.model.DTO.Admin.Blog
+{
+    public class BlogCreateDto
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string PhotoUrl { get; set; }
+
+
+    }
+}
---
+++ AutoPjesaa/AutoPjesaa/model/DTO/Admin/Blog/BlogUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoPjesaa.model.DTO.Admin.Blog
+{
+    public class BlogUpdateDto
+    {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string PhotoUrl { get; set; }
+}
+}
